b5c07c73d92606406aa2e6fa5193a434c3d3e529
bennylevinger <90521983+bennylevinger@users.noreply.github.com>
2021-11-24 16:38:53 +0200

    qq

--- vars/buildDocker.groovy
+++ vars/buildDocker.groovy
@@ -1,14 +1,14 @@
 #!/usr/bin/env groovy
 
-def call(String tag,String workspace, String push , String Dockerfile) {
+def call(String tag,String folder, String push , String Dockerfile) {
   writeFile file: "globalvalues.groovy", text: libraryResource("globalvalues.groovy")
   def values = readProperties file: "globalvalues.groovy"
   dockerRegistry = values['dockerRegistry']
     echo "building and pushing version: ${tag}"
   sh """
-    cd ${workspace}
+    cd ${folder}
     echo building image ${tag}
-    docker build -t ${dockerRegistry}/${tag} -f ${Dockerfile} .
+    docker build -t ${dockerRegistry}/${tag} .
   """
   if ("${push}" == 'true'){
     echo "${push} comes to shove"
